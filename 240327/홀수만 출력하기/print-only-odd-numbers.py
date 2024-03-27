7cfae0ad8726275fb7c5e8ddfe1d757d6936b8c6
SuhunKim <55549043+SuhunKim@users.noreply.github.com>
2024-03-27 23:41:12 +0900

    Title: 홀수만 출력하기, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240327/홀수만 출력하기/print-only-odd-numbers.py
+++ 240327/홀수만 출력하기/print-only-odd-numbers.py
@@ -4,7 +4,8 @@ list_n=[]
 
 for i in range(n):
     list_n.append(int(input()))
-list_n.sort()
+
+#list_n.sort()
 
 for i in range(len(list_n)):
     if list_n[i]%3==0 and list_n[i]%2==1 and list_n[i]!=0:
