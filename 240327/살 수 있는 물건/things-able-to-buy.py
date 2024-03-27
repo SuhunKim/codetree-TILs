fffcd722456da1c9ee7db937214448464dcb8c66
SuhunKim <55549043+SuhunKim@users.noreply.github.com>
2024-03-27 21:41:35 +0900

    Title: 살 수 있는 물건, Time: 188ms, Memory: 24MB, Status: Passed - CodeTree

--- 240327/살 수 있는 물건/things-able-to-buy.py
+++ 240327/살 수 있는 물건/things-able-to-buy.py
@@ -6,7 +6,7 @@ list_p = [["book",3000],["mask",1000]]
 list_p = sorted(list_p,key = lambda x:(-x[1]))
 answer = "no"
 for i in range(len(list_p)):
-    if list_p[i][1]==n:
+    if list_p[i][1]<=n:
         answer = list_p[i][0]
         break
         #print(list_p[i][0])
