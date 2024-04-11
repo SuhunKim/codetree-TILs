18976aebeb09679fe7e466c87ddbbf93ece01ff0
SuhunKim <55549043+SuhunKim@users.noreply.github.com>
2024-04-11 23:33:13 +0900

    Title: 조건에 부합하는 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240411/조건에 부합하는 수/number-that-meets-the-condition.py
@@ -0,0 +1,15 @@
+a = int(input())
+
+ans=[]
+
+for i in range(a+1):
+    if i%2==0 and i%4!=0 :
+        continue
+    elif (i%8)%2==0:
+        continue
+    elif i%7<4:
+        continue
+    else:
+        ans.append(i)
+
+print(' '.join(ans))
