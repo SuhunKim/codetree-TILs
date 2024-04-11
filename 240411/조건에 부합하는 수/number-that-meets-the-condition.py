864c677aa62c76c8a1f7b838f95ee7e7a47507e5
SuhunKim <55549043+SuhunKim@users.noreply.github.com>
2024-04-11 23:51:41 +0900

    Title: 조건에 부합하는 수, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240411/조건에 부합하는 수/number-that-meets-the-condition.py
+++ 240411/조건에 부합하는 수/number-that-meets-the-condition.py
@@ -5,11 +5,11 @@ ans=[]
 for i in range(a+1):
     if i%2==0 and i%4!=0 :
         continue
-    elif (i%8)%2==0:
+    elif (i//8)%2==0:
         continue
     elif i%7<4:
         continue
     else:
-        ans.append(i)
+        ans.append(str(i))
 
 print(' '.join(ans))
